9b4e678f2886638e3e0ac5a724e4f2193127d5a2
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2025-10-02 18:31:31 -0300

    Aula ORM

--- src/main/java/br/cefetrj/dao/UsuarioDAO.java
+++ src/main/java/br/cefetrj/dao/UsuarioDAO.java
@@ -1,10 +1,10 @@
 package br.cefetrj.dao;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
 import br.cefetrj.model.Usuario;
 import br.cefetrj.utils.HibernateUtil;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -14,7 +14,7 @@ public class UsuarioDAO {
         Transaction tx = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             tx = session.beginTransaction();
-            session.save(usuario);
+            session.persist(usuario); // antes: save()
             tx.commit();
         } catch (Exception e) {
             if (tx != null)
@@ -26,6 +26,41 @@ public class UsuarioDAO {
     public List<Usuario> listar() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("from Usuario", Usuario.class).list();
+        }
+    }
+
+    public void atualizar(Usuario usuario) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.merge(usuario); // antes: update()
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void deletar(int id) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            Usuario usuario = session.get(Usuario.class, id);
+            if (usuario != null) {
+                session.remove(usuario); // antes: delete()
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Usuario buscarPorId(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Usuario.class, id);
         }
     }
 }
--- src/main/java/br/cefetrj/model/Usuario.java
+++ src/main/java/br/cefetrj/model/Usuario.java
@@ -1,6 +1,6 @@
 package br.cefetrj.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "usuarios")
