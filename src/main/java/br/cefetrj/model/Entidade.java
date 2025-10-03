8fb9eac8b6b0cf7b49b0d1e92c2886da42ff4cfb
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2025-10-02 21:18:40 -0300

    Aula ORM

--- src/main/java/br/cefetrj/model/Entidade.java
+++ src/main/java/br/cefetrj/model/Entidade.java
@@ -10,4 +10,12 @@ public class Entidade {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
