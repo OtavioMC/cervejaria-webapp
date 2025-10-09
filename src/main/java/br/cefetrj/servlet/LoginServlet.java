4ee6e4dd3db122894be83582e04bc774170ef3bb
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2025-10-09 19:14:53 -0300

    Teste

--- src/main/java/br/cefetrj/servlet/LoginServlet.java
+++ src/main/java/br/cefetrj/servlet/LoginServlet.java
@@ -1,5 +1,15 @@
 package br.cefetrj.servlet;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class LoginServlet {
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Logando...");
+    }
 }
