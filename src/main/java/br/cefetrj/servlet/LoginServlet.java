847deb02b8cce69a78d73d8edba33c71c8da3c7a
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2025-10-09 19:16:23 -0300

    Teste

--- src/main/java/br/cefetrj/servlet/LoginServlet.java
+++ src/main/java/br/cefetrj/servlet/LoginServlet.java
@@ -1,5 +1,14 @@
 package br.cefetrj.servlet;
 
-public class LoginServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Fazendo Login...");
+    }
 }
