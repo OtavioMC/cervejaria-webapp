c8fc574bce2766d83f24d7f1b89ad55a746c099a
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2025-10-23 20:18:12 -0300

    PostmanServlet

--- src/main/java/br/cefetrj/controller/EditoraController.java
+++ src/main/java/br/cefetrj/controller/EditoraController.java
@@ -1,5 +1,9 @@
 package br.cefetrj.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "editora")
+@Api(value = "/api/v1/editoras", tags = { "Editoras - EditoraController" })
 public class EditoraController {
 
 }
